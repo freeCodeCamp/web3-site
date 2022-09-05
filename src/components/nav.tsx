22649c5700f2fd32b1f161603dc9a30062dbd5cf
Shaun Hamilton <shauhami020@gmail.com>
2022-09-05 21:36:31 +0100

    fix: forgotten js -> ts paths

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import FreeCodeCampLogo from './logos/freecodecamp-logo.js';
+import FreeCodeCampLogo from './logos/freecodecamp-logo';
 
 import './nav.css';
 
