558dcad4fb27a27f16e00e81b3868c712e9be8a5
Shaun Hamilton <shauhami020@gmail.com>
2022-11-10 13:34:27 +0000

    feat: add links, 404

--- src/pages/alpha.tsx
+++ src/pages/alpha.tsx
@@ -1,13 +1,30 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import { coursesInfo, courseTitles } from '../static/courses';
+import { FourZeroFour } from './404';
+
+type ParamType = {
+  course: keyof typeof courseTitles;
+};
 
 export const Alpha = () => {
-  const { course } = useParams();
+  const { course } = useParams<ParamType>();
+  const courseInfo = coursesInfo.find(c => c.name === course);
+
+  if (!courseInfo) {
+    return (
+      <>
+        <FourZeroFour path={course || ''} />
+      </>
+    );
+  }
+
   return (
     <main className='main'>
       <section id='instructions'>
         <p>
-          Follow the instructions at <a href={`/${course}`}>/{course}</a>.
+          To test the unreleased {courseInfo.title} courses, follow the
+          instructions at <a href={`/${course}`}>/{course}</a>.
         </p>
         <p>
           After cloning the repo, checkout the <code>next</code> branch to work
@@ -17,14 +34,18 @@ export const Alpha = () => {
       <section>
         <h2>Thanks for being an alpha tester 🎉</h2>
         <p>
-          Please open an issue on GitHub with feedback. All feedback is welcome.
+          Please{' '}
+          <a href={`${courseInfo.repo}/issues/new`} target='_blank'>
+            open an issue on GitHub
+          </a>{' '}
+          with feedback. All feedback is welcome.
         </p>
         <p>Here are some things that would be helpful:</p>
         <ol>
           <li>Was the project accurate?</li>
           <li>Did you enjoy the project?</li>
           <li>Did you struggle with any specific lessons/steps?</li>
-          <li>Did you experience any bugs 🐛</li>
+          <li>Did you experience any bugs 🐛?</li>
         </ol>
       </section>
     </main>
